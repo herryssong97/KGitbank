bec636da8903d9c7fe2bcc57ed4e7a3b3048022d
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2023-03-08 16:19:56 +0900

    Create Primitvetype.java

---
+++ FirstProject/hipkg/Primitvetype.java
@@ -0,0 +1,25 @@
+package hipkg;
+
+public class Primitvetype {
+
+	public static void main(String[] args) {
+
+		/*
+		 * primitive type
+		 * reference type
+		 */	
+		
+		/*
+		 * primitive type (stack 기본형 8가지) => 자료type, 자료길이
+		 * int : 정수, 4바이트 (-2147483648 ~ +2147483647)
+		 * byte : 정수, 1바이트 (-128 ~ +127)
+		 * short : 정수, 2바이트 (-32768 ~ +32767)
+		 * long : 정수, 8바이트 (-000 ~ +000)
+		 * 
+		 */		
+		
+		
+		
+	}
+
+}
