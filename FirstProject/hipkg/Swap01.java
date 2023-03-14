390e443511bc60b233a009715070663e4ae1258d
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2023-03-14 17:25:03 +0900

    Add files via upload

---
+++ FirstProject/hipkg/Swap01.java
@@ -0,0 +1,26 @@
+package hipkg;
+
+public class Swap01 {
+	public static void main(String[] args) {
+
+
+		int imsi;
+		int [] k = {90, 100, 35, 78, 34, 97};
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) { // bubble sort 1´Ü°è
+				
+				if(k[j] < k[j+1]) {
+					imsi = k[j];
+					k[j] = k[j+1];
+					k[j+1] = imsi;
+					
+				}
+			}
+		}
+		
+		for(int p=0; p<k.length; p++) {
+			System.out.print(k[p] + " ");
+		}
+	}
+}
---
+++ FirstProject/hipkg/Swap02.java
@@ -0,0 +1,27 @@
+package hipkg;
+
+public class Swap02 {
+	public static void main(String[] args) {
+
+
+		int imsi;
+		int [] k = {90, 100, 35, 78, 34, 97};
+		// k.length = 6
+		
+		for(int i=0; i<5; i++) { // k.length-1
+			for(int j=0; j<5-i; j++) { // bubble sort 2´Ü°è
+				
+				if(k[j] < k[j+1]) {
+					imsi = k[j];
+					k[j] = k[j+1];
+					k[j+1] = imsi;
+					
+				}
+			}
+		}
+		
+		for(int p=0; p<k.length; p++) {
+			System.out.print(k[p] + " ");
+		}
+	}
+}
