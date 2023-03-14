92e5a35688e16739989c65e2563e5d618a21196e
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2023-03-14 16:32:19 +0900

    Add files via upload

---
+++ FirstProject/eextends/Cast02.java
@@ -0,0 +1,13 @@
+package eextends;
+
+public class Cast02 {
+	public static void main(String[] args) {
+
+		/*
+		 * overriding 재정의 가상메소드 (VIRTUAL) 
+		 * 1) 상속에서 메소드 이름이 같아야 
+		 * 2) 재정의 시 약간 다르게 하는 것
+		 */ 
+		
+	}
+}
